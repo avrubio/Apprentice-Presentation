76639b87c29769147db6ddfe2f1974aeb60cb364
avrubio <ariadna.vanegas@gmail.com>
2024-10-24 17:00:29 -0400

    tailwind and daisyui set up with nav bar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Navbar from './NavBar/Navbar';
+
+function App() {
+  return (
+    <div className="App" data-theme="pastel">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
